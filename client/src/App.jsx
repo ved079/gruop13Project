7bead8dc531b0a2af6c3acf6bf11afa9dde34763
ved079 <127423292+ved079@users.noreply.github.com>
2024-12-18 16:08:54 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,14 +9,12 @@ import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Checkout from "./pages/Checkout";
-import { useSelector } from "react-redux";
 import PlaceOrder from "./pages/PlaceOrder";
 import OrderConfirmation from "./pages/OrderConfirm";
 import { OrderHistory } from "./pages/OrderHistory";
 
 function App() {
-  const userLoginReducer = useSelector((state) => state.userLoginReducer);
-  const { userInfo } = userLoginReducer || {}; // Safeguard against undefined
+  console.log("App without Redux rendered");
 
   return (
     <>
@@ -24,16 +22,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/products/:id" element={<ProductDetail />} />
-          <Route
-            exact
-            path="/login"
-            element={userInfo ? <Navigate to="/" /> : <Login />}
-          />
-          <Route
-            exact
-            path="/register"
-            element={userInfo ? <Navigate to="/" /> : <Register />}
-          />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
           <Route path="/order/:id" element={<OrderConfirmation />} />
           <Route path="/order-history" element={<OrderHistory />} />
           <Route exact path="/checkout" element={<Checkout />} />
